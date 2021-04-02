f18fb620d48ab29d1dd84c06eb18a418427cc281
unknown <ana.maria.ghiorghiu10@gmail.com>
2021-04-02 22:08:31 +0300

    complet

--- Laboratorul 7/2/Vector.h
+++ Laboratorul 7/2/Vector.h
@@ -1,4 +1,10 @@
 #pragma once
+#pragma once
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
 template <class T>
 class Vector
 {
@@ -6,18 +12,173 @@ private:
 	T* e;
 	int nr;
 public:
-	Vector();
-	~Vector();
-	Vector(const Vector&);
-	Vector& operator = (const Vector&);
-	void push(T);
-	T pop();
-	void remove(int);
-	void insert(T, int);
-	//sort method(sort all elements in the vector ==> use a callback function to provide a way to compare two elements.If the callback function is not present(nullptr), operator< is used.)
-	const T& get(int); 
-	void set(int, T);
-	int count();
-	//firstIndexOf method(given an object and a callback function, find the first index of the element from the vector that is equal(according to the callback function) with the given parameter.If the callback function is nor present(nullptr), operator== is used.
+	Vector() {
+		e = NULL;
+		nr = 0;
+	}
+	~Vector() {
+		if (e != NULL)
+			free(e);
+	}
+	Vector(const Vector& v) {
+		int i;
+		if (v.e == NULL)
+		{
+			e = NULL;
+			nr = 0;
+		}
+		else
+		{
+			e = (T*)malloc(sizeof(*v.e));
+			nr = v.nr;
+			for (i = 0; i < nr; i++)
+				e[i] = v.e[i];
+		}
+	}
+	Vector& operator = (const Vector& v) {
+		int i;
+		if (this != &v)
+		{
+			if (e != NULL)
+				free(e);
+			nr = v.nr;
+			if (v.e == NULL)
+				e = NULL;
+			else
+			{
+				e = (T*)malloc(sizeof(*v.e));
+				for (i = 0; i < nr; i++)
+					e[i] = v.e[i];
+			}
+		}
+
+		return *this;
+	}
+	void push(T x) {
+		T* aux;
+		int i;
+		if (e == NULL)
+		{
+			e = (T*)malloc(sizeof(T));
+			e[0] = x;
+			nr = 1;
+		}
+		else
+			if (sizeof(*e) / sizeof(T) > nr)
+			{
+				e[nr] = x;
+				nr++;
+			}
+			else
+			{
+				aux = (T*)malloc(2 * sizeof(*e));
+				for (i = 0; i < nr; i++)
+					aux[i] = e[i];
+				aux[nr] = x;
+				nr++;
+				free(e);
+				e = aux;
+			}
+	}
+	T pop() {
+		return e[nr - 1];
+	}
+	void remove(int index) {
+		int i;
+		if (index >= 0 && index <= nr - 1)
+		{
+			for (i = index + 1; i <= nr; i++)
+				e[i - 1] = e[i];
+			nr--;
+		}
+	}
+	void insert(T x, int index)
+	{
+		int i;
+		T* aux;
+		if (index >= 0 && index <= nr)
+		{
+			if (e == NULL)
+				e = (T*)malloc(sizeof(T));
+			else
+				if (sizeof(*e) / sizeof(T) == nr)
+				{
+					aux = (T*)malloc(2 * sizeof(*e));
+					for (i = 0; i < nr; i++)
+						aux[i] = e[i];
+					free(e);
+					e = aux;
+				}
+			for (i = nr - 1; i >= index; i--)
+				e[i + 1] = e[i];
+			e[index] = x;
+			nr++;
+		}
+	}
+	void sort(bool (*mai_mic)(T, T))
+	{
+		bool ordonat;
+		T aux;
+		int i;
+		do
+		{
+			ordonat = true;
+			for (i = 0; i < nr - 1; i++)
+				if (mai_mic == NULL)
+				{
+					if (e[i + 1] < e[i])
+					{
+						ordonat = false;
+						aux = e[i];
+						e[i] = e[i + 1];
+						e[i + 1] = aux;
+					}
+				}
+				else
+					if (mai_mic(e[i + 1], e[i]))
+					{
+						ordonat = false;
+						aux = e[i];
+						e[i] = e[i + 1];
+						e[i + 1] = aux;
+					}
+		} while (!ordonat);
+	}
+	const T& get(int index)
+	{
+		return e[index];
+	}
+	void set(int index, T x)
+	{
+		if (index >= 0 && index <= nr - 1)
+			e[index] = x;
+	}
+	int count() {
+		return nr;
+	}
+	void Print()
+	{
+		int i;
+		cout << "[";
+		for (i = 0; i < nr; i++)
+			cout << e[i] << ",";
+		if (nr > 0)
+			cout << "\b";
+		cout << "]";
+	}
+	int firstIndexOf(T x, bool (*egal)(T, T))
+	{
+		int i;
+		for (i = 0; i < nr; i++)
+			if (egal == NULL)
+			{
+				if (e[i] == x)
+					return i;
+			}
+			else
+				if (egal(e[i], x))
+					return i;
+		return -1;
+	}
 };
 
--- Laboratorul 7/2/main.cpp
+++ Laboratorul 7/2/main.cpp
@@ -1,9 +1,88 @@
-#include <iostream>
 #include "Vector.h"
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
+	Vector<int> v;
+	int nr_o;
+	int x;
+	int index;
+	do
+	{
+		cout << "1.Push		 5.Set			  9. Sort" << endl;
+		cout << "2.Insert	 6.Get			  10.Print" << endl;
+		cout << "3.Remove	 7.Count		  0.Exit" << endl;
+		cout << "4.Pop		 8.FirstIndexOf" << endl;
+		cout << "Number of desired option:";
+		cin >> nr_o;
+		cout << endl;
+		switch (nr_o)
+		{
+		case 1:
+			cout << "Value:";
+			cin >> x;
+			v.push(x);
+			break;
+		case 2:
+			cout << "Value:";
+			cin >> x;
+			cout << "Position:";
+			cin >> index;
+			v.insert(x, index);
+			break;
+		case 3:
+			cout << "Position:";
+			cin >> index;
+			v.remove(index);
+			break;
+		case 4:
+			if (v.count() == 0)
+				cout << "Vector is empty!" << endl;
+			else
+				cout << v.pop() << endl;
+			break;
+		case 5:
+			cout << "Value:";
+			cin >> x;
+			cout << "Position:";
+			cin >> index;
+			v.set(index, x);
+			break;
+		case 6:
+			cout << "Position:";
+			cin >> index;
+			if (index >= 0 && index < v.count())
+				cout << v.get(index);
+			else
+				cout << "Position is incorrect!";
+			cout << endl;
+			break;
+		case 7:
+			cout << "Number of elements:" << v.count() << endl;
+			break;
+		case 8:
+			cout << "Value:";
+			cin >> x;
+			index = v.firstIndexOf(x, NULL);
+			if (index == -1)
+				cout << "Value was not found!" << endl;
+			else
+				cout << "First index of " << x << " is " << index << endl;
+			break;
+		case 9:
+			v.sort(NULL);
+			break;
+		case 10:
+			v.Print();
+			cout << endl;
+			break;
+		default:
+			if (nr_o != 0)
+				cout << "Option does not exist!" << endl;
+		}
+		cout << endl;
+	} while (nr_o != 0);
 	return 0;
 }
